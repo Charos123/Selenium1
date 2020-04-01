88855bb0367b8011d616681c15392efbbffe6d01
charossharipova <charos_sharipova@yahoo.com>
2020-04-01 13:09:59 -0400

    TestBase

--- src/test/java/com/cybertek/base/TestBase.java
+++ src/test/java/com/cybertek/base/TestBase.java
@@ -1,19 +1,19 @@
 package com.cybertek.base;
 
+import com.cybertek.utilities.Driver;
+
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.sql.Driver;
-
 public abstract class  TestBase {
   protected WebDriver driver;
 
-     @BeforeMethod
-    public void setupMethod(){
-       driver=WebDriverFactory.getDriver("chrome");
-     }
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = Driver.getDriver();
+    }
       @AfterMethod
     public void tearDownMethod() throws InterruptedException {
          Thread.sleep(5000);
